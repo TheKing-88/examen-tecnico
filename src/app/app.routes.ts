3c41e36f118f1e782fc689bbcffcc91d3f1f9274
Carlos <carlos@dev.s-tech.com.mx>
2021-04-01 12:58:26 -0600

    modificaciones en los modulos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,33 +3,12 @@ import { RouterModule, Routes } from '@angular/router'
 //ruta principal de mi aplicacion
 import { PagesComponent } from './pages/pages.component';
 
-import { ListComponent } from './pages/list/list.component';
-import { GraphComponent } from './pages/graph/graph.component'; 
 import { LoginComponent } from './login/login.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
 
 
 const appRoutes: Routes = [
-    {
-        path: '',
-        component: PagesComponent,
-        children: [
-            {
-                path: 'list',
-                component: ListComponent
-            },
-            {
-                path: 'graph',
-                component: GraphComponent
-            },
-            {
-                path: '',
-                redirectTo: '/list',
-                pathMatch: 'full'
-            },
-        ]
-    },
     {
         path: 'login',
         component: LoginComponent
